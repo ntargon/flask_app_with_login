c625db8da16448dc0356bf275f8425e0e49d451e
ntargon <argon19951128@gmail.com>
2022-09-24 23:16:49 +0900

    tailwind.config.cjsの対応

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-  purge: ["./index.html",'./src/**/*.{svelte,js,ts}'], // for unused CSS
+  content: ["./index.html",'./src/**/*.{svelte,js,ts}'], // for unused CSS
 }
