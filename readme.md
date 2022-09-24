53a28d6d454a65e8833ca934ab92f84b84e2fdc1
ntargon <argon19951128@gmail.com>
2022-09-24 11:02:54 +0900

    tmp

--- readme.md
+++ readme.md
@@ -1,1 +1,26 @@
 # ログイン機能を備えたflaskアプリのテスト
+
+
+本番時は`waitress`を使う
+
+
+実行方法
+
+**開発時**
+
+at terminal 1
+```
+cd client/; yarn vite; # terminal 1
+```
+at terminal 2
+```
+export FLASK_CONFIG_FILE_PATH=development.cfg; python server/server.py;
+```
+
+**本番時**
+
+```
+cd client/; yarn build;
+cd ../;
+export FLASK_CONFIG_FILE_PATH=production.cfg; python server/server.py;
+```
--- server/server.py
+++ server/server.py
@@ -1,23 +1,35 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, redirect
 import random
+from waitress import serve
 
 app = Flask(__name__)
+app.config.from_envvar('FLASK_CONFIG_FILE_PATH')
+PORT = app.config['PORT']
 
 # Path for our main Svelte page
 @app.route("/")
 def base():
-    return send_from_directory('../client/dist', 'index.html')
+    return send_from_directory(app.config['SVELTE_MAIN_PAGE_DIR'], 'index.html')
 
 # Path for all the static files (compiled JS/CSS, etc.)
 @app.route("/<path:path>")
 def home(path):
-    return send_from_directory('../client/dist', path)
-
+    if app.config['ENVIRONMENT'] == 'PRODUCTION':
+        return send_from_directory(app.config['SVELTE_MAIN_PAGE_DIR'], path)
+    elif app.config['ENVIRONMENT'] == 'DEVELOPMENT':
+        # viteを使う
+        return redirect("http://localhost:5173/" + path)
+    else:
+        assert(False)
 
 @app.route("/rand")
 def hello():
     return str(random.randint(0, 100))
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    if app.config['ENVIRONMENT'] == 'PRODUCTION':
+        serve(app, host='0.0.0.0', port=PORT)
+    elif app.config['ENVIRONMENT'] == 'DEVELOPMENT':
+        app.run(port=5050)
+    else:
+        assert(False)
